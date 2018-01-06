a0a37290c77632137f11c05e5b0be080800bba3b
Marten Siiber <martensiiber@gmail.com>
2018-01-07 00:25:40 +0200

    Added unit tests for text drawing

--- src/utils/Canvas.js
+++ src/utils/Canvas.js
@@ -1,8 +1,109 @@
-export function writeText(context, text) {
-  let start = 0;
-  text.split('').forEach((letter) => {
-    context.fillText(letter, start, 0);
-    start += context.measureText(letter).width;
-  });
-  // console.log(context.measureText(' '));
+export function writeText(canvasContext, content, textOptions = {lineHeight: 20, paragraphSpace: 5}) {
+  const canvasWidth = canvasContext.canvas.width;
+  const text = content.getPlainText('\n').replace(/\n/g, '');
+  let currentBlock = content.getFirstBlock();
+  let currentBlockKey = currentBlock.getKey();
+  let characterMetadata = currentBlock.getCharacterList();
+  let blockLength = currentBlock.getText().length;
+  let previousBlocksLength = 0;
+  const defaultStyle = canvasContext.font;
+  /*
+    WordMetadata: [word, startPosition, endPosition, lineNumber][]
+  */
+  const wordMetadata = [];
+  const letters = text.split('');
+  const textLength = letters.length;
+  let currentStyle = defaultStyle;
+  let spaceWidth = canvasContext.measureText(' ').width;
+  let currentWord = '';
+  let wordStartPosition = 0;
+  let fillXStart = 0;
+  let fillYStart = textOptions.lineHeight;
+  let fillText = '';
+
+  const nextBlock = () => {
+    previousBlocksLength += blockLength;
+    currentBlock = content.getBlockAfter(currentBlockKey);
+    currentBlockKey = currentBlock.getKey();
+    characterMetadata = currentBlock.getCharacterList();
+    blockLength = currentBlock.getText().length;
+  };
+
+  const newLine = ({additionalSpacing = 0, text = ''}) => {
+    fillXStart = 0;
+    fillYStart += textOptions.lineHeight + additionalSpacing;
+    fillText = text;
+    wordStartPosition = 0;
+  };
+
+  const addWordMetadata = () => {
+    const wordWidth = canvasContext.measureText(currentWord).width;
+    wordMetadata.push([currentWord, wordStartPosition, wordStartPosition + wordWidth, fillYStart]);
+    currentWord = '';
+  };
+  // console.log(content.getBlockMap().toArray()[0].getCharacterList());
+
+  letters.forEach(
+    (currentLetter, letterIndex) => {
+      if (letterIndex === blockLength) {
+        // Block ended
+        nextBlock();
+        canvasContext.fillText(fillText, fillXStart, fillYStart);
+        addWordMetadata();
+        newLine({additionalSpacing: textOptions.paragraphSpace});
+      }
+      const nextStyle = characterMetadata.get(letterIndex - previousBlocksLength).getStyle();
+      if (currentStyle !== nextStyle) {
+        // Style changed
+        // Draw previous styled block
+        canvasContext.fillText(fillText, fillXStart, fillYStart);
+        const fillTextWidth = canvasContext.measureText(fillText).width;
+        fillXStart = fillTextWidth;
+        fillText = currentLetter;
+        // Update with new style
+        canvasContext.font = getCanvasFont(defaultStyle, nextStyle);
+        spaceWidth = canvasContext.measureText(' ').width;
+        currentStyle = nextStyle;
+      } else {
+        fillText += currentLetter;
+      }
+      if (currentLetter === ' ') {
+        // Word ended
+        // Check if it fits in current line
+        const wordWidth = canvasContext.measureText(currentWord).width;
+        if (wordStartPosition + wordWidth > canvasWidth) {
+          fillText = fillText.substring(0, fillText.substring(0, fillText.length - 1).lastIndexOf(' '));
+          canvasContext.fillText(fillText, fillXStart, fillYStart);
+          newLine({text: currentWord + ' '});
+        }
+        addWordMetadata();
+        wordStartPosition = wordStartPosition + spaceWidth + wordWidth;
+      } else {
+        currentWord += currentLetter;
+        if (letterIndex + 1 === textLength) {
+          // Text ended
+          // Draw all remaining text
+          const fillTextWidth = canvasContext.measureText(fillText).width;
+          canvasContext.fillText(fillText, fillXStart, fillYStart);
+          fillXStart = fillTextWidth;
+          fillText = currentLetter;
+          // Add remaining word metadata
+          addWordMetadata();
+          wordStartPosition = 0;
+        }
+      }
+    }
+  );
+  return wordMetadata;
+}
+
+function getCanvasFont(defaultStyle, nextStyle) {
+  let result = defaultStyle;
+  if (nextStyle.contains('BOLD')) {
+    result = 'bold ' + result;
+  }
+  if (nextStyle.contains('ITALIC')) {
+    result = 'italic ' + result;
+  }
+  return result;
 }
--- test/CanvasTest.js
+++ test/CanvasTest.js
@@ -1,14 +1,17 @@
 import {writeText} from '../src/utils/Canvas';
+import {ContentState, convertFromHTML} from 'draft-js';
 
 describe('Canvas', () => {
   const expectedCanvas = document.createElement('canvas');
   const actualCanvas = document.createElement('canvas');
   const diffCanvas = document.createElement('canvas');
-  const canvasWidth = 200;
-  const canvasHeight = 40;
+  const canvasWidth = 250;
+  const canvasHeight = 50;
   const fontSize = 16;
+  const lineHeight = fontSize + 4;
+  const paragraphSpace = 10;
   const font = 'Calibri';
-  const textBaseline = 'top';
+  const textBaseline = 'bottom';
   let expectedContext = expectedCanvas.getContext('2d');
   let actualContext = actualCanvas.getContext('2d');
   let diffContext = diffCanvas.getContext('2d');
@@ -30,13 +33,15 @@ describe('Canvas', () => {
 
   afterEach(function() {
     // console.log(this.currentTest);
-    const data = diffCanvas.toDataURL().replace(/^data:image\/\w+;base64,/, '');
-    const buffer = new Buffer(data, 'base64');
-    fs.writeFile(__dirname + '/output/' + this.currentTest.title + '.png', buffer, (err) => {
-      if(err) return console.log(err);
-    });
+    // Reset any applied styling
+    expectedContext.font = fontSize + 'pt ' + font; expectedContext.textBaseline = textBaseline;
+    actualContext.font = fontSize + 'pt ' + font; actualContext.textBaseline = textBaseline;
+    diffContext.font = fontSize + 'pt ' + font; diffContext.textBaseline = textBaseline;
+    outputPNG(diffCanvas, __dirname + '/output/diff/' + this.currentTest.title + '.png');
     if (this.currentTest.state !== 'passed') {
       // console.log(this.currentTest);
+      outputPNG(expectedCanvas, __dirname + '/output/expected/' + this.currentTest.title + '.png');
+      outputPNG(actualCanvas, __dirname + '/output/actual/' + this.currentTest.title + '.png');
     }
   });
 
@@ -45,35 +50,155 @@ describe('Canvas', () => {
   });
 
   it('passes similarity test', () => {
-    expectedContext.fillText('expected', 0, 0);
-    actualContext.fillText('expected', 0, 0);
-    const expectedImage = expectedContext.getImageData(0, 0, canvasWidth, canvasHeight);
-    const actualImage = actualContext.getImageData(0, 0, canvasWidth, canvasHeight);
-    const diffImage = diffContext.createImageData(canvasWidth, canvasHeight);
-    const pixelDiff = pixelmatch(expectedImage.data, actualImage.data, diffImage.data, canvasWidth, canvasHeight);
-    diffContext.putImageData(diffImage, 0, 0);
-    expect(pixelDiff).to.equal(0);
+    expectedContext.fillText('expected', 0, lineHeight);
+    actualContext.fillText('expected', 0, lineHeight);
+    expect(getPixelDifference(expectedContext, actualContext, diffContext)).to.equal(0);
   });
 
   it('passes difference test', () => {
-    expectedContext.fillText('expected', 0, 0);
-    actualContext.fillText('actual', 0, 0);
-    const expectedImage = expectedContext.getImageData(0, 0, canvasWidth, canvasHeight);
-    const actualImage = actualContext.getImageData(0, 0, canvasWidth, canvasHeight);
-    const diffImage = diffContext.createImageData(canvasWidth, canvasHeight);
-    const pixelDiff = pixelmatch(expectedImage.data, actualImage.data, diffImage.data, canvasWidth, canvasHeight, {threshold: 0.01});
-    diffContext.putImageData(diffImage, 0, 0);
-    expect(pixelDiff).not.to.equal(0);
+    expectedContext.fillText('expected', 0, lineHeight);
+    actualContext.fillText('actual', 0, lineHeight);
+    expect(getPixelDifference(expectedContext, actualContext, diffContext)).not.to.equal(0);
   });
 
   it('passes letter-by-letter test', () => {
-    expectedContext.fillText('letter-by-letter', 0, 0);
-    writeText(actualContext, 'letter-by-letter');
+    expectedContext.fillText('letter-by-letter', 0, lineHeight);
+    const content = ContentState.createFromText('letter-by-letter');
+    writeText(actualContext, content, {lineHeight: lineHeight});
+    expect(getPixelDifference(expectedContext, actualContext, diffContext)).to.equal(0);
+  });
+
+  it('passes bold style test', () => {
+    expectedContext.font = 'bold ' + expectedContext.font;
+    expectedContext.fillText('bold test', 0, lineHeight);
+    const content = getDraftJSContentFromHTML('<b>bold test</b>');
+    writeText(actualContext, content, {lineHeight: lineHeight});
+    expect(getPixelDifference(expectedContext, actualContext, diffContext)).to.equal(0);
+  });
+
+  it('passes italic style test', () => {
+    expectedContext.font = 'italic ' + expectedContext.font;
+    expectedContext.fillText('italic test', 0, lineHeight);
+    const content = getDraftJSContentFromHTML('<i>italic test</i>');
+    writeText(actualContext, content, {lineHeight: lineHeight});
+    expect(getPixelDifference(expectedContext, actualContext, diffContext)).to.equal(0);
+  });
+
+  it('passes bold italic style test', () => {
+    expectedContext.font = 'bold italic ' + expectedContext.font;
+    expectedContext.fillText('bold italic test', 0, lineHeight);
+    const content = getDraftJSContentFromHTML('<b><i>bold italic test</i></b>');
+    writeText(actualContext, content, {lineHeight: lineHeight});
+    expect(getPixelDifference(expectedContext, actualContext, diffContext)).to.equal(0);
+  });
+
+  it('passes style change test', () => {
+    const defaultStyle = expectedContext.font;
+    expectedContext.font = 'bold ' + expectedContext.font;
+    expectedContext.fillText('style', 0, lineHeight);
+    const position = expectedContext.measureText('style').width;
+    expectedContext.font = defaultStyle;
+    expectedContext.fillText(' change test', position, lineHeight);
+    const content = getDraftJSContentFromHTML('<b>style</b> change test');
+    writeText(actualContext, content, {lineHeight: lineHeight});
+    expect(getPixelDifference(expectedContext, actualContext, diffContext)).to.equal(0);
+  });
+
+  it('passes text wrap test', () => {
+    expectedContext.fillText('this is a multiline text that', 0, lineHeight);
+    expectedContext.fillText('should be wrapped', 0, lineHeight + lineHeight);
+    const content = getDraftJSContentFromHTML('this is a multiline text that should be wrapped');
+    writeText(actualContext, content, {lineHeight: lineHeight});
+    expect(getPixelDifference(expectedContext, actualContext, diffContext)).to.equal(0);
+  });
+
+  it('passes multiple paragraph test', () => {
+    expectedContext.fillText('paragraph1 text', 0, lineHeight);
+    expectedContext.fillText('paragraph2 text', 0, lineHeight + paragraphSpace + lineHeight);
+    const content = getDraftJSContentFromHTML('<p>paragraph1 text</p><p>paragraph2 text</p>');
+    writeText(actualContext, content, {lineHeight: lineHeight, paragraphSpace: paragraphSpace});
+    expect(getPixelDifference(expectedContext, actualContext, diffContext)).to.equal(0);
+  });
+
+  it('returns single word metadata', () => {
+    const content = getDraftJSContentFromHTML('metadata');
+    const singleWordWidth = expectedContext.measureText('metadata').width;
+    const expectedTextMetadata = [
+      ['metadata', 0, singleWordWidth, lineHeight]
+    ];
+    const actualTextMetadata = writeText(actualContext, content, {lineHeight: lineHeight});
+    expect(actualTextMetadata).to.eql(expectedTextMetadata);
+  });
+
+  it('returns multiple words metadata', () => {
+    const content = getDraftJSContentFromHTML('multiple word metadata');
+    const firstWordWidth = expectedContext.measureText('multiple').width;
+    const secondWordWidth = expectedContext.measureText('word').width;
+    const thirdWordWidth = expectedContext.measureText('metadata').width;
+    const spaceWidth = expectedContext.measureText(' ').width;
+    const secondWordStart = firstWordWidth + spaceWidth;
+    const thirdWordStart = secondWordStart + secondWordWidth + spaceWidth;
+    const expectedTextMetadata = [
+      ['multiple', 0, firstWordWidth, lineHeight],
+      ['word', secondWordStart, secondWordStart + secondWordWidth, lineHeight],
+      ['metadata', thirdWordStart, thirdWordStart + thirdWordWidth, lineHeight]
+    ];
+    const actualTextMetadata = writeText(actualContext, content, {lineHeight: lineHeight});
+    expect(actualTextMetadata).to.eql(expectedTextMetadata);
+  });
+
+  it('returns multiple line metadata', () => {
+    const content = getDraftJSContentFromHTML('multiple word metadata that should be wrapped');
+    const firstWordWidth = expectedContext.measureText('should').width;
+    const secondWordWidth = expectedContext.measureText('be').width;
+    const thirdWordWidth = expectedContext.measureText('wrapped').width;
+    const spaceWidth = expectedContext.measureText(' ').width;
+    const secondWordStart = firstWordWidth + spaceWidth;
+    const thirdWordStart = secondWordStart + secondWordWidth + spaceWidth;
+    const expectedTextMetadata = [
+      ['should', 0, firstWordWidth, lineHeight + lineHeight],
+      ['be', secondWordStart, secondWordStart + secondWordWidth, lineHeight + lineHeight],
+      ['wrapped', thirdWordStart, thirdWordStart + thirdWordWidth, lineHeight + lineHeight]
+    ];
+    const actualTextMetadata = writeText(actualContext, content, {lineHeight: lineHeight});
+    expect(actualTextMetadata).to.include.deep.members(expectedTextMetadata);
+  });
+
+  it('returns multiple paragraph metadata', () => {
+    const content = getDraftJSContentFromHTML('<p>paragraph1 text</p><p>paragraph2 text</p>');
+    const firstWordWidth = expectedContext.measureText('paragraph1').width;
+    const secondWordWidth = expectedContext.measureText('text').width;
+    const spaceWidth = expectedContext.measureText(' ').width;
+    const secondWordStart = firstWordWidth + spaceWidth;
+    const expectedTextMetadata = [
+      ['paragraph1', 0, firstWordWidth, lineHeight],
+      ['text', secondWordStart, secondWordStart + secondWordWidth, lineHeight],
+      ['paragraph2', 0, firstWordWidth, lineHeight + paragraphSpace + lineHeight],
+      ['text', secondWordStart, secondWordStart + secondWordWidth, lineHeight + paragraphSpace + lineHeight]
+    ];
+    const actualTextMetadata = writeText(actualContext, content, {lineHeight: lineHeight, paragraphSpace: paragraphSpace});
+    expect(actualTextMetadata).to.eql(expectedTextMetadata);
+  });
+
+  function getPixelDifference(expectedContext, actualContext, diffContext) {
     const expectedImage = expectedContext.getImageData(0, 0, canvasWidth, canvasHeight);
     const actualImage = actualContext.getImageData(0, 0, canvasWidth, canvasHeight);
     const diffImage = diffContext.createImageData(canvasWidth, canvasHeight);
     const pixelDiff = pixelmatch(expectedImage.data, actualImage.data, diffImage.data, canvasWidth, canvasHeight);
     diffContext.putImageData(diffImage, 0, 0);
-    expect(pixelDiff).to.equal(0);
-  });
+    return pixelDiff;
+  }
+
+  function outputPNG(canvas, filePath) {
+    const data = canvas.toDataURL().replace(/^data:image\/\w+;base64,/, '');
+    const buffer = new Buffer(data, 'base64');
+    fs.writeFile(filePath, buffer, (err) => {
+      if(err) return console.log(err);
+    });
+  }
+
+  function getDraftJSContentFromHTML(html) {
+    const blocksFromHTML = convertFromHTML(html);
+    return ContentState.createFromBlockArray(blocksFromHTML.contentBlocks, blocksFromHTML.entityMap);
+  }
 });
