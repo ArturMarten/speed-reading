fc70b2671c18942a1521f74d5ced53241f77cc3b
Marten Siiber <martensiiber@gmail.com>
2018-07-26 19:33:50 +0300

    Added empty credentials config

---
+++ src/credentials.js
@@ -0,0 +1,11 @@
+const credentials = {
+  admin: {
+    username: '', // Unit testing
+    password: '', // Unit testing
+  },
+  demo: {
+    username: '', // Demo account authentication
+  },
+};
+
+export default credentials;
