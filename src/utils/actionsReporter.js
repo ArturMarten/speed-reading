1ecf1e09efed2b2fa067a6dc27e42d8b76b1fe5f
Marten Siiber <martensiiber@gmail.com>
2018-09-26 12:42:08 +0000

    Fixed stashing issue.

--- src/utils/actionsReporter.js
+++ src/utils/actionsReporter.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 export const actionData = {
   actions: [],
 
@@ -21,27 +20,3 @@ const actionsReporter = () => next => (action) => {
 };
 
 export default actionsReporter;
-=======
-export const actionData = {
-  actions: [],
-
-  addAction: function addAction(action) {
-    this.actions.push(action);
-  },
-
-  getActions: function getActions(length = this.actions.length) {
-    return this.actions
-      .filter(action => action.type.indexOf('@@localize/ADD_TRANSLATION') === -1)
-      .slice(Math.max(this.actions.length - length - 1, 0));
-  },
-};
-
-const actionsReporter = () => next => (action) => {
-  if (typeof action === 'object') {
-    actionData.addAction(action);
-  }
-  return next(action);
-};
-
-export default actionsReporter;
->>>>>>> Stashed changes
